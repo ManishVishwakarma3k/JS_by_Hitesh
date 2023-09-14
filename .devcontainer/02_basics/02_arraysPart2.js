1a0b5bf71b1e12686237e0430f114f9aa5b3c66c
ManishVishwakarma3k <87070301+ManishVishwakarma3k@users.noreply.github.com>
2023-09-14 16:49:15 +0000

    Working on array concatenation

---
+++ .devcontainer/02_basics/02_arraysPart2.js
@@ -0,0 +1,14 @@
+const marvelHeroes = ["Ironman", "Thor", "Hulk", "Spiderman"]
+const dcHeroes = ["Superman", "Batman", "Aquaman", "Flash"]
+
+marvelHeroes.push(dcHeroes)
+marvelHeroes.pop()
+
+// console.log(marvelHeroes);
+// console.log(marvelHeroes[4][3]);
+
+const allHeroes = marvelHeroes.concat(dcHeroes)
+// console.log(allHeroes);
+
+console.log(marvelHeroes);
+console.log(dcHeroes);
