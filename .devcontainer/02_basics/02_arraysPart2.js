ad92bac52cef1b508720a6cb5034c097a0743858
ManishVishwakarma3k <87070301+ManishVishwakarma3k@users.noreply.github.com>
2023-09-15 07:33:10 +0000

    Learnt concat, spread, flaten, isArray, from and of arrays

--- .devcontainer/02_basics/02_arraysPart2.js
+++ .devcontainer/02_basics/02_arraysPart2.js
@@ -7,8 +7,25 @@ marvelHeroes.pop()
 // console.log(marvelHeroes);
 // console.log(marvelHeroes[4][3]);
 
-const allHeroes = marvelHeroes.concat(dcHeroes)
+const allHeroes = marvelHeroes.concat(dcHeroes)// Allows 2 arrays to merge
 // console.log(allHeroes);
 
-console.log(marvelHeroes);
-console.log(dcHeroes);
+const allHeroesNew = [...marvelHeroes, ...dcHeroes] // Allows multiple arrays to merge and spreads the elements
+// console.log(allHeroesNew);
+
+const numberArray = [1, 2, 3, [4, 5, 6], 7, 8, [9 , 10, 11, [12, 13, 14], 15, 16]]
+const convertedNumberArray = numberArray.flat(Infinity)
+// console.log(convertedNumberArray); // Flatens array based level provided in brackets.
+
+// console.log(numberArray);
+
+console.log(Array.isArray(["Manish", "Vishwakarma"]));
+console.log(Array.from("Ironman"));
+console.log(Array.from({fName: "Manish"})); // If cannot convert to array returns empty parenthesis
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
