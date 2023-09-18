10352faca807bce6dc78f3d9f63fe8d67d37ec66
ManishVishwakarma3k <87070301+ManishVishwakarma3k@users.noreply.github.com>
2023-09-18 17:13:45 +0000

    Ongoing

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -24,7 +24,7 @@ function addTwoNumbers(number1, number2){ //Parameters
 const results = addTwoNumbers(10, 25)
 //console.log(`The value of addition function is ${results}`);
 
-function userLoginMessage(username){
+function userLoginMessage(username = "You have"){//"You have" default parameter if argument is not passed
     if(!username){
         console.log("Please enter a your username");
         return
@@ -32,5 +32,26 @@ function userLoginMessage(username){
     return `${username} logged in successfully.`
 }
 
-console.log(userLoginMessage("Manish"));
+// console.log(userLoginMessage());
 
+function calculateCartPrice(val1, val2, ...num1){//...rest operator packages multiple values and sends it as an array
+    return num1
+}
+
+// console.log(calculateCartPrice(100, 200, 400, 800)); //val1 takes 100, val2 takes 200, remaining are displayed as array
+
+const basicUser = {
+    username: "barnacle",
+    bookprice: "1999"
+}
+
+function handleUser(anyUser){
+    return `${anyUser.bookprice} is the value to be paid by ${anyUser.username} for checkout.`
+}
+
+console.log(handleUser(basicUser));
+
+console.log(handleUser({
+    username: "Prajwal",
+    bookprice: "10999"
+}));
