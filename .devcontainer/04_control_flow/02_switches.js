45790dd51d037ca7a392bf59c519d2f707a74017
ManishVishwakarma3k <87070301+ManishVishwakarma3k@users.noreply.github.com>
2023-09-24 08:07:48 +0000

    Switch case

---
+++ .devcontainer/04_control_flow/02_switches.js
@@ -0,0 +1,50 @@
+// switch (key) { // Syntax od switch case
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const randomNumber = 5
+
+// switch (randomNumber) {
+//     case 1:
+//         console.log(`Value match the random number between 1 to 5 at ${randomNumber}`);
+//         break;
+//     case 2:
+//         console.log(`Value match the random number between 1 to 5 at ${randomNumber}`);
+//         break;
+//     case 3:
+//         console.log(`Value match the random number between 1 to 5 at ${randomNumber}`);
+//         break;
+//     case 4:
+//         console.log(`Value match the random number between 1 to 5 at ${randomNumber}`);
+//         break;
+//     case 5:
+//         console.log(`Value match the random number between 1 to 5 at ${randomNumber}`);
+//         break;
+
+//     default:
+//         console.log(`Value doesn't match the random number between the range 1 to 5`);
+//         break;
+// }
+
+const randomMonth = "Hello"
+
+switch (randomMonth) {
+    case "Jan":
+        console.log(`Value match the random month of the year at ${randomMonth}`);
+        break;
+    case "Feb":
+        console.log(`Value match the random month of the year at ${randomMonth}`);
+        break;
+    case "Mar":
+        console.log(`Value match the random month of the year at ${randomMonth}`);
+        break;
+    
+    default:
+        console.log(`Value not within first quater of the year i.e. from Jan to Mar`);
+        break;
+}
