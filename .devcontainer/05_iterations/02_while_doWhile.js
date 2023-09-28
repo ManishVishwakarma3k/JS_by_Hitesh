1157c784d1edebb186acae805da8a8c1bc3dbf48
ManishVishwakarma3k <87070301+ManishVishwakarma3k@users.noreply.github.com>
2023-09-28 22:13:37 +0000

    While, Do While

---
+++ .devcontainer/05_iterations/02_while_doWhile.js
@@ -0,0 +1,27 @@
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of Index is ${index}`);
+//     index = index + 3
+// }
+
+let superHeroes = ["Ironman", "Hulk", "Gamora", "Scarlett Witch", "Vision"]
+
+let arrIndex = 0
+
+while (arrIndex < superHeroes.length) {
+    // console.log(`Superheroes in array are ${superHeroes[arrIndex]}`);
+    arrIndex++
+}
+
+let score = 1
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+let score1 = 11
+do {
+    console.log(`The unexpected Score is ${score1}`);
+    score++
+} while (score1 <= 10);
